164b18c319a094d0766ed847543e85d5a92d0b7a
downside154 <blue_tree154@hotmail.com>
2021-03-30 14:47:06 +0900

    feat: first commit created class initializer with initial values for Dice Created .gitignore file to ignore brainstorming files

---
+++ dice_game02.py
@@ -0,0 +1,26 @@
+import random
+
+class User:
+
+    def __init__ (a, b , c)= "__main__"
+
+
+class Table:
+
+    def __init__ (a, b , c)= "__main__"
+
+
+class Dice: 
+    def __init__(values):
+        self.allDice = ("Reroll", "Reroll", "Attack", "Attack", "Shield", "Skull")
+        
+
+
+
+
+
+
+
+
+
+
