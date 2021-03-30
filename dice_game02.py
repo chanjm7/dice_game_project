cbad713ae054107649955ed6a66259c08ccbaf2b
downside154 <blue_tree154@hotmail.com>
2021-03-30 14:50:07 +0900

    feat: creted dice result separator
    created a dice roll result separator function to separetely count the
    results of the dice roll. To be implemented on Table to work
    sequentially.

--- dice_game02.py
+++ dice_game02.py
@@ -18,6 +18,25 @@ class Dice:
     def dice_roll_results():
         roll_value = random.choice(self.allDice)
         
+@Dice
+def roll_separation(value):
+    Reroll = 0
+    Attack = 0
+    Shield = 0
+    Skull = 0
+
+    for i in roll_values:
+        if i == "Reroll":
+            Reroll += 1
+        elif i == "Attack":
+            Attack += 1
+        elif i == "Shield":
+            Shield += 1
+        elif i == "Skull":
+            Skull += 1
+
+    print (f"Reroll: {Reroll}, Shield: {Shield}, Skull: {Skull}, Attack: {Attack}")
+
 
 
 
